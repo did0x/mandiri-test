861ec3c40310db57745474988d10eb68636ac953
Ridho Saputra <hiputraa@gmail.com>
2023-03-23 10:35:19 +0700

    Add base class

---
+++ app/src/main/java/co/saputra/mandiritest/base/BasePagingAdapter.kt
@@ -0,0 +1,25 @@
+package co.saputra.mandiritest.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+abstract class BasePagingAdapter<T : Any, VB: ViewBinding, VH: RecyclerView.ViewHolder>(
+    private val layoutInflater: LayoutInflater,
+    private val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB,
+    diffCallback: DiffUtil.ItemCallback<T>
+) : PagingDataAdapter<T, VH>(diffCallback) {
+
+    protected abstract fun itemViewHolder(
+        viewBinding: VB,
+        viewType: Int,
+    ) : VH
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val viewBinding = bindingInflater.invoke(layoutInflater, parent, false)
+        return itemViewHolder(viewBinding, viewType)
+    }
+}
---
+++ app/src/main/java/co/saputra/mandiritest/base/BaseViewModel.kt
@@ -0,0 +1,44 @@
+package co.saputra.mandiritest.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.putrash.common.Event
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val _isLoading = MutableLiveData<Event<Boolean>>()
+    val isLoading: LiveData<Event<Boolean>> get() = _isLoading
+
+    protected fun showLoading() {
+        _isLoading.postValue(Event(true))
+    }
+
+    protected fun hideLoading() {
+        _isLoading.postValue(Event(false))
+    }
+
+    private val _errorMessage = MutableLiveData<Event<String>>()
+    val errorMessage: LiveData<Event<String>> get() = _errorMessage
+
+    protected fun showError(message: String) {
+        _errorMessage.postValue(Event(message))
+    }
+
+    inline fun <T> launchPagingAsync(
+        crossinline execute: suspend () -> Flow<T>,
+        crossinline onSuccess: (Flow<T>) -> Unit
+    ) {
+        viewModelScope.launch {
+            try {
+                val result = execute()
+                onSuccess(result)
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+        }
+    }
+}
